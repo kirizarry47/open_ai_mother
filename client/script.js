6ef413ff8fe410b00dab7dbc521fc908dd7d2b96
kirizarry47 <68522693+kirizarry47@users.noreply.github.com>
2023-02-08 13:23:46 -0500

    Created file structure and css rules.

---
+++ client/script.js
@@ -0,0 +1,16 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_contatiner');
+
+let loadInterval;
+
+function loader(element) {
+    element.textContent = '';
+
+    loadInterval = setInterval(() =>{
+        //Update the text content
+    })
+}
